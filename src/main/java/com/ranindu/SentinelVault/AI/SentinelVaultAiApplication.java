e805a992b5e39f660ff47a77394dc0443f1d25c3
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-03-23 20:15:21 +0530

    feat: initial commit with spring boot structure

---
+++ src/main/java/com/ranindu/SentinelVault/AI/SentinelVaultAiApplication.java
@@ -0,0 +1,13 @@
+package com.ranindu.SentinelVault.AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelVaultAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelVaultAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranindu/SentinelVault/AI/SentinelVaultAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranindu.SentinelVault.AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelVaultAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
